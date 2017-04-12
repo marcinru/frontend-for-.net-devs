5d582d518cddd300d173d58b7e18fbc247231aec
Marcin Rudzki <mar.rudzki@gmail.com>
2017-04-12 21:13:21 +0200

    Add form id="add-todo-form"

--- examples/jquery/js/todo.jquery.js
+++ examples/jquery/js/todo.jquery.js
@@ -2,5 +2,11 @@ $(function () {
   $('#username').change(function () {
     let username = $(this).find('option:selected').text();
     $('#current-user').text(username);
+  });
+
+  $('#add-todo-form').submit(function (event) {
+    event.preventDefault();
+    let taskName = $('#task-name');
+    console.log(taskName.val());
   })
 })
